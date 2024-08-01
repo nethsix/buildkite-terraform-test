d140380459ee100d1b06073415e0bb7cd8cac0d3
nethsix <nethsix@gmail.com>
2024-08-01 12:34:58 +0900

    Add buildkite publish script

---
+++ .buildkite/steps/publish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euo pipefail
+
+OIDC_TOKEN=$(buildkite-agent oidc request-token --audience "https://packages.buildkite.com/anothertest/starter-pipeline" --lifetime 300)
+curl -X POST https://api.buildkite.com/v2/packages/organizations/anothertest/registries/myterra/packages -H "Authorization: Bearer $OIDC_TOKEN" -F "file=@ksh_all-1.0.4.tgz"
