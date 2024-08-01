f78a1ddb631384165a55a91bd2f6e57744ee0f41
nethsix <nethsix@gmail.com>
2024-08-01 12:40:36 +0900

    Add echo debug into publish.sh

--- .buildkite/steps/publish.sh
+++ .buildkite/steps/publish.sh
@@ -2,4 +2,5 @@
 set -euo pipefail
 
 OIDC_TOKEN=$(buildkite-agent oidc request-token --audience "https://packages.buildkite.com/anothertest/starter-pipeline" --lifetime 300)
+echo "Hello $OIDC_TOKEN"
 curl -X POST https://api.buildkite.com/v2/packages/organizations/anothertest/registries/myterra/packages -H "Authorization: Bearer $OIDC_TOKEN" -F "file=@ksh_all-1.0.4.tgz"
