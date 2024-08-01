f53b5e469c099b3b07e87b8453745e50a97fc67b
nethsix <nethsix@gmail.com>
2024-08-01 12:54:02 +0900

    Tweak publish.sh script

--- .buildkite/steps/publish.sh
+++ .buildkite/steps/publish.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 OIDC_TOKEN=$(buildkite-agent oidc request-token --audience "https://buildkite.com/organizations/anothertest/packages/registries/myterra" --lifetime 300)
 echo "Hello $OIDC_TOKEN"
-curl -X POST https://api.buildkite.com/v2/packages/organizations/anothertest/registries/myterra/packages -H "Authorization: Bearer $OIDC_TOKEN" -F "file=@ksh_all-1.0.4.tgz"
+curl -X POST https://api.buildkite.com/v2/packages/organizations/anothertest/registries/myterra/packages -H "Authorization: Bearer ${OIDC_TOKEN}" -F "file=@ksh_all-1.0.4.tgz"
